76a828b11dd25416ed2277e0a6443c81a0326f72
Abel <aatsou12@gmail.com>
2024-03-08 10:05:26 -0800

    done

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,16 +5,16 @@
 def pascal_triangle(n):
     """function returning a pascal triangle array based on taken input"""
     if n <= 0:
-        return (0)
+        return ([[]])
     if n == 1:
-        return ([1])
+        return ([[1]])
     pascal_triangle_array = [[1], [1, 1]]
     base = [1, 1]
     for i in range(2, n):
         new = []
-        for h, n in  enumerate(base):
+        for h, n in enumerate(base):
             if (h + 1 < len(base)):
-                new.append(n +  base[h + 1])
+                new.append(n + base[h + 1])
         nb = [1]
         for n in new:
             nb.append(n)
