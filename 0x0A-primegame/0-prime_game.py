a619a7e251ab9247341ed319fec093c5a81c6ac5
Abel <aatsou12@gmail.com>
2024-05-16 07:50:36 -0700

    done

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -39,20 +39,17 @@ def isWinner(x, nums):
                 return nums[i]
         return -1
 
-    # Main game logic
     winners = {'Maria': 0, 'Ben': 0}
 
     for n in nums:
-        # Counting the number of primes in the given range
         primes_count = sum(1 for num in range(1, n + 1) if is_prime(num))
 
-        # If the number of primes is even, Ben wins. Otherwise, Maria wins.
         if primes_count % 2 == 0:
             winners['Ben'] += 1
         else:
             winners['Maria'] += 1
 
-    # Determining the winner with the most wins
+    # Determining the winner
     if winners['Maria'] == winners['Ben']:
         return None
     elif winners['Maria'] > winners['Ben']:
