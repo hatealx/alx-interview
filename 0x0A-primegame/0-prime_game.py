103ffdcb6dbe8a12af6721239a38838caedbb3bb
Abel <aatsou12@gmail.com>
2024-05-16 07:40:08 -0700

    done

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -32,22 +32,27 @@ def isWinner(x, nums):
         str or None: The name of the player that won the most rounds.
         If the winner cannot be determined, returns None.
     """
+    # Function to find the next prime number in the set
     def find_next_prime(current, nums):
         for i in range(current + 1, len(nums)):
             if is_prime(nums[i]):
                 return nums[i]
         return -1
 
+    # Main game logic
     winners = {'Maria': 0, 'Ben': 0}
 
     for n in nums:
+        # Counting the number of primes in the given range
         primes_count = sum(1 for num in range(1, n + 1) if is_prime(num))
+
+        # If the number of primes is even, Ben wins. Otherwise, Maria wins.
         if primes_count % 2 == 0:
             winners['Ben'] += 1
         else:
             winners['Maria'] += 1
 
-    # Determining the winner 
+    # Determining the winner with the most wins
     if winners['Maria'] == winners['Ben']:
         return None
     elif winners['Maria'] > winners['Ben']:
